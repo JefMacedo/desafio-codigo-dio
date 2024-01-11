4a8ca8d79414911d4cdcc977e428d0bdad1c53e5
Jeferson Macedo <jhef.salles@gmail.com>
2024-01-11 17:14:43 -0300

    Implementado de acordo com os requisitos

--- ObjetoComAtributos/Program.cs
+++ ObjetoComAtributos/Program.cs
@@ -2,13 +2,26 @@
 
 class Jogador
 {
-    public string Nome { get; set; }
-    public string Nacionalidade { get; set; }
-    public int Idade { get; set; }
-    public string Posicao { get; set; }
+    private string nome;
+    private string nacionalidade;
+    private int idade;
+    private string posicao;
 
-    public Jogador()
+    public Jogador(string nome, string nacionalidade, int idade, string posicao)
     {
+        this.nome = nome;
+        this.nacionalidade = nacionalidade;
+        this.idade = idade;
+        this.posicao = posicao;
+    }
+
+    public void ExibirInfo()
+    {
+        Console.WriteLine("Jogador criado!");
+        Console.WriteLine(nome);
+        Console.WriteLine(nacionalidade);
+        Console.WriteLine(idade);
+        Console.WriteLine("Posição: " + posicao);
     }
 }
 
@@ -16,6 +29,20 @@ class Program
 {
     static void Main()
     {
+        Console.Write("Digite o nome do jogador: ");
+        string nome = Console.ReadLine();
 
+        Console.Write("Digite a nacionalidade do jogador: ");
+        string nacionalidade = Console.ReadLine();
+
+        Console.Write("Digite a idade do jogador: ");
+        int idade = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite a posição do jogador: ");
+        string posicao = Console.ReadLine();
+
+        Jogador jogador = new Jogador(nome, nacionalidade, idade, posicao);
+
+        jogador.ExibirInfo();
     }
 }
