e4a6fe7b42e9d96100b43e8876d8858ea3625cc6
Jeferson Macedo <jhef.salles@gmail.com>
2024-01-12 13:18:37 -0300

    Implementado de acordo com a documentação

--- Pink&Cerebro/Program.cs
+++ Pink&Cerebro/Program.cs
@@ -1,28 +1,22 @@
-﻿//Abaixo segue um exemplo de código que você pode ou não utilizar
-using System; 
+﻿using System;
 
-class minhaClasse {
+int tamanhoArr = int.Parse(Console.ReadLine());
+string[] numeroArr = Console.ReadLine().Split(' ');
 
-    static void Main(string[] args) { 
+int multiplo2 = 0;
+int multiplo3 = 0;
+int multiplo4 = 0;
+int multiplo5 = 0;
 
-        int suavariavel = int.Parse(Console.ReadLine());
-        string[] n      = Console.ReadLine().Split(' ');
+for (int iContador = 0; iContador < tamanhoArr; iContador++)
+{
+    if ((int.Parse(numeroArr[iContador]) %  2) == 0) {multiplo2++;}
+    if ((int.Parse(numeroArr[iContador]) %  3) == 0) {multiplo3++;}
+    if ((int.Parse(numeroArr[iContador]) %  4) == 0) {multiplo4++;}
+    if ((int.Parse(numeroArr[iContador]) %  5) == 0) {multiplo5++;}
+}
 
-        int multiplo2   = 0;
-        int multiplo3   = 0;
-        int multiplo4   = 0;
-        int multiplo5   = 0;
-//TODO: Complete os espaços em branco com uma possível solução para o desafio
-        for (int iContador = 0;                        )
-        {
-            if ((int.Parse(n[iContador]) %  ) == 0)                 ;
-            if ((int.Parse(n[iContador]) %  ) == 0)                 ;
-            if ((int.Parse(n[iContador]) %  ) == 0)                 ;
-            if ((int.Parse(n[iContador]) %  ) == 0)                 ;
-        }
-
-        Console.WriteLine("{0} Multiplo(s) de 2", multiplo2);
-        Console.WriteLine("{0} Multiplo(s) de 3", multiplo3);
-        Console.WriteLine("{0} Multiplo(s) de 4", multiplo4);
-        Console.WriteLine("{0} Multiplo(s) de 5", multiplo5);
-    }
+Console.WriteLine($"{multiplo2} Multiplo(s) de 2");
+Console.WriteLine($"{multiplo3} Multiplo(s) de 3");
+Console.WriteLine($"{multiplo4} Multiplo(s) de 4");
+Console.WriteLine($"{multiplo5} Multiplo(s) de 5");
