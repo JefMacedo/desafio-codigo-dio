0373f128a4d207c8731af163bcb0e552fa2287f4
Jeferson Macedo <jhef.salles@gmail.com>
2024-01-12 12:53:24 -0300

    Implementação da solução conforme a documentação

--- MultiplicacaoSimples/Program.cs
+++ MultiplicacaoSimples/Program.cs
@@ -1,13 +1,7 @@
 ﻿using System;
 
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int a, b;
-            a = Convert.ToInt32(Console.ReadLine());
-            b = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("PROD = " + (            )); //implemente o código que representa a multiplicação.
-            Console.ReadLine();
-        }
-    }
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int prod = a * b;
+Console.WriteLine($"PROD = {prod}");
+Console.ReadLine();
